6d41110746cf7c2543efd3b5ceb4e1da53825eb7
Om Patil <patil4om@gmail.com>
2025-12-31 15:23:38 +0530

    Added error logs

--- middleware/logs/logger.js
+++ middleware/logs/logger.js
@@ -1,19 +1,18 @@
 import winston from 'winston';
 const { combine, timestamp, json } = winston.format;
 
-const customLevels = {
-  error: 0,
-  warn: 1,
-  info: 2,
-  http: 3
-};
+// const customLevels = {
+//   error: 0,
+//   warn: 1,
+//   info: 2,
+//   http: 3
+// };
 const logger = winston.createLogger({
-  level: customLevels,
+  level: 'http',
   format: combine(timestamp(), json()),
   transports: [
     new winston.transports.File({
-      filename: './middleware/logs/combined.log',
-      level: 'http'  
+      filename: './middleware/logs/combined.log'
     }),
     new winston.transports.File({
       filename: './middleware/logs/error.log',
